7f23ed99260001e610bad17009a28860d6cfeef3
dogmatic69 <dogmatic69@gmail.com>
2011-10-06 23:00:57 +0100

    adding the code to attach the drafted behavior auto from events

--- history_events.php
+++ history_events.php
@@ -24,12 +24,16 @@
 		 * @return void
 		 */
 		public function onAttachBehaviors($event = null) {
-			if(!$event->Handler->shouldAutoAttachBehavior() || empty($event->Handler->revision) || !$event->Handler->revision) {
+			if(!$event->Handler->shouldAutoAttachBehavior()) {
 				return false;
 			}
 
-			if ($event->Handler->shouldAutoAttachBehavior('History.Revision')) {
+			if((!empty($event->Handler->revision) && $event->Handler->revision) && $event->Handler->shouldAutoAttachBehavior('History.Revision')) {
 				$this->__attachHistory($event);
+			}
+
+			if((!empty($event->Handler->drafted) && $event->Handler->drafted) && $event->Handler->shouldAutoAttachBehavior('History.Drafted')) {
+				$this->__attachDrafted($event);
 			}
 		}
 
@@ -52,13 +56,28 @@
 				user_error($notice, E_USER_NOTICE);
 			}
 
-			return $event->Handler->Behaviors->attach(
-				'History.Revision',
-				array(
-					'ignore' => array(
-						'views'
-					)
-				)
-			);
+			return $event->Handler->Behaviors->attach('History.Revision', Configure::read('History.behaviorConfig'));
+		}
+
+		/**
+		 * @brief attach the drafted behavior to models that require it.
+		 *
+		 * @access private
+		 *
+		 * @param Event $event the event that was triggered
+		 *
+		 * @return bool see BehaviorCollection::attach()
+		 */
+		private function __attachDrafted($event) {
+			if(Configure::read('debug') && !in_array($event->Handler->table . Configure::read('Drafted.suffix'), $event->Handler->getTables())) {
+				$notice = sprintf(
+					__d('history', 'Trying to attatch Drafted to %s (%s) but there is no draft table', true),
+					prettyName($event->Handler->alias),
+					$event->Handler->name
+				);
+				user_error($notice, E_USER_NOTICE);
+			}
+
+			return $event->Handler->Behaviors->attach('History.Drafted', Configure::read('Drafted.behaviorConfig'));
 		}
 	}
