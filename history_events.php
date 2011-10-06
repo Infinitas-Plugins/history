a79268b984ca163e3c20f8481d44ce972683a752
dogmatic69 <dogmatic69@gmail.com>
2011-10-06 19:42:09 +0100

    adding the event to attach revisions auto from the event callbacks

--- history_events.php
+++ history_events.php
@@ -1,4 +1,50 @@
 <?php
 	class HistoryEvents extends AppEvents {
-		
+		/**
+		 * @brief attach behaviors to models
+		 *
+		 * @access public
+		 *
+		 * @param Event $event the event that was triggered
+		 *
+		 * @return void
+		 */
+		public function onAttachBehaviors($event = null) {
+			if(!$event->Handler->shouldAutoAttachBehavior() || empty($event->Handler->revision) || !$event->Handler->revision) {
+				return false;
+			}
+
+			if ($event->Handler->shouldAutoAttachBehavior('History.Revision')) {
+				$this->__attachHistory($event);
+			}
+		}
+
+		/**
+		 * @brief attach the revision behavior to models that require it.
+		 *
+		 * @access private
+		 *
+		 * @param Event $event the event that was triggered
+		 *
+		 * @return bool see BehaviorCollection::attach()
+		 */
+		private function __attachHistory($event) {
+			if(Configure::read('debug') && !in_array($event->Handler->table . '_rev', $event->Handler->getTables())) {
+				$notice = sprintf(
+					__d('history', 'Trying to attatch Revisioning to %s (%s) but there is no revision table', true),
+					prettyName($event->Handler->alias),
+					$event->Handler->name
+				);
+				user_error($notice, E_USER_NOTICE);
+			}
+
+			return $event->Handler->Behaviors->attach(
+				'History.Revision',
+				array(
+					'ignore' => array(
+						'views'
+					)
+				)
+			);
+		}
 	}
